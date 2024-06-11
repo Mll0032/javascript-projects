34ef75bd846117b08b12328f9bf1766f29d91a23
mll0032 <mllawson18@gmail.com>
2024-06-11 17:09:43 -0500

    Print typeof

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -5,7 +5,11 @@ let distanceToMarsKm = 225000000;
 let distanceToMoonKm = 384400;
 let milesPerKilometer = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof milesPerKilometer);
 // Calculate a space mission below
 
 // Print the results of the space mission calculations below
