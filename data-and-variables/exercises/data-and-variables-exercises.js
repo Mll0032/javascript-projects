d3ed8f917953181b10489411d4a23609bcc4a16c
mll0032 <mllawson18@gmail.com>
2024-06-11 17:16:52 -0500

    Print Results days to reach Mars

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -15,7 +15,7 @@ let milesToMars = distanceToMarsKm*milesPerKilometer;
 let hoursToMars = milesToMars/shuttleSpeedMph;
 let daysToMars = hoursToMars/24;
 // Print the results of the space mission calculations below
-
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.");
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
