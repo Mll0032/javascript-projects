c9daa646024d26f75f9ed52769ea0fb0a12547b8
mll0032 <mllawson18@gmail.com>
2024-06-18 21:22:14 -0500

    Madlib template literals

--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,7 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "wookies" ;
+let name = "Boba Fett";
+let verb = "fall in love";
+let adjective = "silly";
+let color = "green";
 
-console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun} — that allows ${name} to ${verb} with strings.`)
