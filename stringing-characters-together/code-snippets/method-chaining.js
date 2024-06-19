9fa0bdce6affe55676ce37fc7cec19e08b149349
mll0032 <mllawson18@gmail.com>
2024-06-18 21:39:06 -0500

    Playing with string methods

--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -6,6 +6,14 @@ console.log(word.toUpperCase());
 //Returns ``JAVASCRIPT``
 
 //What does ``word.slice(4).toUpperCase()`` return?
-
+//This returns SCRIPT
 
 //Experiment with other combinations (chains) of string methods.
+console.log(word.toLowerCase());
+console.log(word.replace('J', 'Y'));
+
+
+let coffee = word.slice(0,4);
+let writing = word.slice(4,10);
+let combinedWords = `${coffee}${writing}`;
+console.log(combinedWords);
