9e87f6b24d9dec41c3ee46763d2cc284b3c78c12
mll0032 <mllawson18@gmail.com>
2024-06-18 21:15:22 -0500

    Working with string index and template literals

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -1,4 +1,7 @@
 let jsCreator = "Brendan Eich";
 
-console.log(jsCreator[-1]);
-console.log(jsCreator[42]);
+let firstInitial = jsCreator[0];
+let lastInitial = jsCreator[8];
+
+let outputStr = `JavaScript was created by somebody with initials ${firstInitial}.${lastInitial}.`;
+ console.log(outputStr);
