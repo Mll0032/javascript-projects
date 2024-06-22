b054447ed68c88dac96933fe765b0a2b64cb495b
mll0032 <mllawson18@gmail.com>
2024-06-21 23:11:10 -0500

    Excercises 1 and 2

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,22 +3,64 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+//node for-Loop-Exercises.js
+//a.
+//for (let i = 0; i < 21; i++) {
+//  console.log(i);
+//}
 
+//b.
+//for (let i = 3; i < 30; i = i + 2) {
+//  console.log(i);
+//}
 
+//c.
+//for (let i = 12; i > -15; i = i - 2) {
+//  console.log(i);
+//}
 
+//d. 
+//for (let i = 50; i > 20; i--) {
+//  if(i % 3 === 0)
+//  console.log(i);
+//}
 
 /*Exercise #2: 
+
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
 
 
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+let string = "LaunchCode";
+let reversed = "";
+let array1 = [1, 5, 'LC101', 'blue', 42];
 
+//a.
+for (let i = 0; i < array1.length; i++) {
+  console.log(array1[i]);
+}
 
-
-
+//b.
+for (let i = 0; i < string.length; i++) {
+  reversed = string[i] +reversed;
+}
+ console.log(reversed);
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+let arr1 = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let arrEven = [];
+let arrOdd = [];
+
+for (i = 0; i < arr1.length; i++)
+    if (arr1[i] % 2 === 0) {
+      arrEven.push(arr1[i]);
+    }else {
+      arrOdd.push(arr1[i]);
+    }
+console.log("Even Numbers: ", arrEven);
+console.log("Odd Numbers: ", arrOdd);
