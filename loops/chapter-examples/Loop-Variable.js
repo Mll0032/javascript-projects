4b1185a829a8ebc67db6aa72c15ce3e65e425700
mll0032 <mllawson18@gmail.com>
2024-06-21 22:46:47 -0500

    Loop variables

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,16 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
-   console.log(i);
- }
+//for (let i = 0; i < 51; i++) {
+  // console.log(i);
+ //}
+
+
+ //Chaning the number where I is initialized just changes the start position of the countup
+ //for (let i = 2; i < 51; i++) {
+ // console.log(i);
+//}
+
+//this created a forever loop since 0 is always going to be less that 51 the conditions will bever be false so it will contniue until there is no memory left in the computer 
+for (let i = 0; i < 51; i--) {
+  console.log(i);
+  }
