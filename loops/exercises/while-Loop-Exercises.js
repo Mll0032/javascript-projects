0445d8be7e1dbcf635ddf9eae4865b4dc74653b5
mll0032 <mllawson18@gmail.com>
2024-06-22 19:43:26 -0500

    Monitor fuel and altitude and if achieved or failed to achieve orbit

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -3,8 +3,7 @@ const input = require('readline-sync');
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 let fuelLevel = "";
 let numOfAstnts = "";
-let shuttleAlt = "";
-
+let shuttleAlt = 0;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
@@ -16,12 +15,47 @@ let shuttleAlt = "";
           fuelLevel = Number(input.question("You cannot have negative or non-numeric fuel! Please resubmit fuel level: "));
       } else if (fuelLevel < 5000) {
           fuelLevel = Number(input.question("You do not have enough fuel! Please resubmit fuel level: "));
-      } else if (fuelLevel >= 30000) {
+      } else if (fuelLevel > 30000) {
           fuelLevel = Number(input.question("You have too much fuel! Please resubmit fuel level: "));
       }
   }
   
   console.log(`Valid starting fuel level: ${fuelLevel}`);
+
+//b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
+const maxNumAstnts = 7;
+numOfAstnts = Number(input.question("How many astronauts are there onboard? "));
+
+while (numOfAstnts <= 1 || numOfAstnts > 7 || isNaN(numOfAstnts)) {
+  if (numOfAstnts <= 0 || isNaN(numOfAstnts)) {
+    numOfAstnts = Number(input.question("You cannot have negative or non-numeric number of astronauts! Please resubmit: "));
+} else if (numOfAstnts > 7) {
+  let numToEject = numOfAstnts - maxNumAstnts
+    numOfAstnts = Number(input.question(`You have too many astonauts onboard! Please eject ${numToEject}: `));
+}
+}
+
+console.log(`You have the perfect ammount of astronauts: ${numOfAstnts}`);
+  
+//c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
+while (fuelLevel > 0 & numOfAstnts > 0) {
+  shuttleAlt += 50;
+  fuelLevel -= 100*numOfAstnts
+}
+
+/*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
+
+If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+
+console.log(`The shuttle gained an altitude of ${shuttleAlt} km.`)
+if (shuttleAlt >= 2000){
+console.log("Orbit achieved!");
+} else {
+  console.log("Failed to reach orbit.");
+}
+
+
+
 
 //using seperate while loops
 //while (fuelLevel <=0){
@@ -53,25 +87,3 @@ let shuttleAlt = "";
 //}else {
 //  console.log("Not enough Fuel!");
 //}
-//b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-const maxNumAstnts = 7;
-numOfAstnts = Number(input.question("How many astronauts are there onboard? "));
-
-while (numOfAstnts <= 0 || numOfAstnts > 7 || isNaN(numOfAstnts)) {
-  if (numOfAstnts <= 0 || isNaN(numOfAstnts)) {
-    numOfAstnts = Number(input.question("You cannot have negative or non-numeric number of astronauts! Please resubmit: "));
-} else if (numOfAstnts > 7) {
-  let numToEject = numOfAstnts - maxNumAstnts
-    numOfAstnts = Number(input.question(`You have too many astonauts onboard! Please eject ${numToEject}: `));
-}
-}
-
-console.log(`You have the perfect ammount of astronauts: ${numOfAstnts}`);
-  
-//c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
-
-
-/*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
-
-If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
