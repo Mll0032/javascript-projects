09256063370dca9ae7421578df36102adb940319
mll0032 <mllawson18@gmail.com>
2024-07-15 16:51:25 -0500

    Unit Testing dump

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,29 @@
 
+//populate launcode object
 let launchcode = {
-  
-}
+  organization: 'nonprofit',
+  executiveDirector: 'Jeff',
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  //creat a function to check if num is divisible by 2 and 3 and only 2 and only 3 and only 5
+  launchOutput: function(num) {
+    if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+      return 'LaunchCode Rocks!';
+    } else if (num % 2 === 0 && num % 3 === 0 && num % 5 !== 0) {
+      return 'LaunchCode!';
+    } else if (num % 3 === 0 && num % 5 === 0 && num % 2 !== 0) {
+      return 'Code Rocks!';
+    } else if (num % 2 === 0 && num % 5 === 0 && num % 3 !== 0) {
+      return 'Launch Rocks!';
+    } else if (num % 2 === 0 && num % 3 !== 0 && num % 5 !== 0) {
+      return 'Launch!';
+    } else if (num % 3 === 0 && num % 2 !== 0 && num % 5 !== 0) {
+      return 'Code!';
+    } else if (num % 5 === 0 && num % 2 !== 0 && num % 3 !== 0) {
+      return 'Rocks!';
+    }
+  }
+};
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,60 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
-
+describe('launchcode object', function() {
   // Write your unit tests here!
-  
+  it("should have an 'organization' property with value 'nonprofit'", function() {
+    expect(launchcode.organization).toBe('nonprofit');
+  });
+  it("should have an 'executiveDirector' property with value 'Jeff'", function() {
+    expect(launchcode.executiveDirector).toBe('Jeff');    
+  });
+  it("should have a 'percentageCoolEmployees' property with value 100", function() {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+  it("should have a 'programsOffered' property with correct array values", function() {
+    expect(launchcode.programsOffered).toEqual(["Web Development", "Data Analysis", "Liftoff"]);
+  });
+  describe('launchOutput method', function() {
+    it("should return 'Launch!' when passed a number only divisible by 2", function() {
+      expect(launchcode.launchOutput(2)).toBe('Launch!');
+      expect(launchcode.launchOutput(4)).toBe('Launch!');
+    });
+  })
+  it("should return 'Code!' when passed a number only divisible by 3", function() {
+    expect(launchcode.launchOutput(3)).toBe('Code!');
+    expect(launchcode.launchOutput(9)).toBe('Code!');
+  });
+  it("should return 'Rocks!' when passed a number only divisible by 5", function() {
+    expect(launchcode.launchOutput(5)).toBe('Rocks!');
+    expect(launchcode.launchOutput(25)).toBe('Rocks!');
+  });
+  it("should return 'LaunchCode!' when passed a number divisible by both 2 and 3", function() {
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+    expect(launchcode.launchOutput(12)).toBe('LaunchCode!');
+  });
+  it("should return 'Code Rocks!' when passed a number divisible by both 3 and 5", function() {
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+    expect(launchcode.launchOutput(45)).toBe('Code Rocks!');
+  });
+  it("should return 'Launch Rocks!' when passed a number divisible by both 2 and 5", function() {
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks!');
+    expect(launchcode.launchOutput(20)).toBe('Launch Rocks!');
+  });
+  it("should return 'LaunchCode Rocks!' when passed a number divisible by 2, 3, and 5", function() {
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+    expect(launchcode.launchOutput(60)).toBe('LaunchCode Rocks!');
+  });
 });
+
+
+
+//trying to use an arrow
+// describe("Testing launchcode", () => {
+//   Write your unit tests here!
+//   it("should have an 'organization' property with value 'nonprofit'", () => {
+//     expect(launchcode.organization).toBe('nonprofit');
+//   });
+// });
+  
+  
