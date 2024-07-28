6989125174df814835e773c6eb82a39419e6de40
mll0032 <mllawson18@gmail.com>
2024-07-28 00:06:50 -0500

    Adding in readline-sync and data validation

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,26 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+const input = require('readline-sync');
+
+let numerator = Number(input.question('Enter Numerator: '));
+let denominator = Number(input.question('Enter Denominator: '));
+
+// function divide(numerator, denominator) {
+//     if (denominator === 0){
+//         throw Error('You cannot divde by zero!');
+//     }else{
+//      return numerator/denominator;
+//     }
+// }
+
+
+
+function divide(numerator, denominator) {
+    while (denominator === 0){
+      console.log('You cannot divde by zero! Please enter a valid number: ');
+      denominator = Number(input.question('Enter Denominator:'));
+    }
+return numerator/denominator;
+}
+console.log(divide(numerator, denominator));
